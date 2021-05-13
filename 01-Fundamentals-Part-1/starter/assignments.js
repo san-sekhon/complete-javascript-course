da054b507909e5ee3144a3a8d5cfa93a0aa6868b
Sandeep Sekhon <sandysss79@gmail.com>
2021-05-12 17:51:35 -0700

    finished guess_my_number

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -5,3 +5,11 @@ console.log(country);
 console.log(continent);
 console.log(population);
 console.log('Hello World!');
+
+const markHeight = 1.69;
+const markWeight = 78;
+const johnHeight = 1.95;
+const johnWeight = 95;
+const markBMI = markWeight / (markHeight * markHeight);
+const johnBMI = johnWeight / (johnHeight * johnHeight);
+const markHigherBMI = markBMI > johnBMI;
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,61 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+//PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTemAmplitude = function (temps) {
+  
+  let max = temps[0];
+  let min = temps[0];
+  for(let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if(typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  return max - min;
+};
+const amplitude = calcTemAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTemAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  return max - min;
+};
+const amplitudeNew = calcTemAmplitudeNew([3,5,1], [9,0, 5]);
+console.log(amplitudeNew);
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC  in ${i + 1} days...
+    `;
+  }
+  console.log('...' + str);
+};
+printForecast(data1);
